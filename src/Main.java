0bc8bdf59e9d5d71330c8c25ab57d9b4ea6c0e4e
Andrey Fedorov <fvandre@outlook.com>
2023-02-22 18:34:26 +0300

    Add java code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int cash = 20455;
+        int cashForOneMile = 20;
+        int miles = cash / cashForOneMile;
+
+        System.out.println(("Вам начислено ") + miles + (" миль"));
+
+
+    }
 }
